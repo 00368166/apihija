682da389a466091fa2824aa207163f7bfbb05233
00368166 <manuel.florescr@anahuac.mx>
2023-05-23 12:08:37 -0600

    a
    a

---
+++ hija/hija/Controllers/hija.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace madre.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class hija : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Saludar()
+        {
+            return Ok("Bienvenido al servicio de la hija");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObtenerDato(int id)
+        {
+            // Obtener y retornar el dato con el ID especificado para la hija
+            // ...
+
+            // Ejemplo: Crear un objeto JSON
+            var dato = new { Id = id, Nombre = "Ejemplo para la hija" };
+
+            return Ok(dato);
+        }
+
+
+        [HttpPost]
+        public IActionResult CrearDato([FromBody] DatoModelo dato)
+        {
+            // Crear el nuevo dato utilizando la información recibida en el cuerpo de la solicitud para la hija
+            // ...
+
+            return Ok("Dato creado exitosamente para la hija");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult ActualizarDato(int id, [FromBody] DatoModelo dato)
+        {
+            // Actualizar el dato con el ID especificado utilizando la información recibida en el cuerpo de la solicitud para la hija
+            // ...
+
+            return Ok("Dato actualizado exitosamente para la hija");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult EliminarDato(int id)
+        {
+            // Eliminar el dato con el ID especificado para la hija
+            // ...
+
+            return Ok("Dato eliminado exitosamente para la hija");
+        }
+
+        // Agrega métodos adicionales específicos para la hija si es necesario
+
+        // ...
+        [HttpPost("ejecutar-programa")]
+        public IActionResult EjecutarPrograma()
+        {
+            try
+            {
+                var startInfo = new ProcessStartInfo
+                {
+                    FileName = "notepad.exe",
+                    CreateNoWindow = true,
+                    UseShellExecute = false
+                };
+
+                using (var process = new Process { StartInfo = startInfo })
+                {
+                    process.Start();
+                    process.WaitForInputIdle();
+
+                    var processId = process.Id;
+
+                    return Ok(new
+                    {
+                        ProcessId = processId,
+                        Message = $"El programa Notepad se ha iniciado correctamente con el PID {processId}."
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new
+                {
+                    Message = $"Error al ejecutar el programa: {ex.Message}"
+                });
+            }
+        }
+
+        [HttpDelete("cerrar-programa/{pid}")]
+        public IActionResult CerrarPrograma(int pid)
+        {
+            try
+            {
+                var process = Process.GetProcessById(pid);
+                var processName = process.ProcessName;
+                var uptime = DateTime.Now - process.StartTime;
+                var alive = DateTime.Now;
+
+                process.Kill();
+
+                var response = new
+                {
+                    Message = $"Programa '{processName}' cerrado exitosamente",
+                    Uptime = uptime,
+                    Alive = alive
+                };
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { Message = $"Error al cerrar el programa: {ex.Message}" });
+            }
+        }
+
+
+        [HttpPost("apagar")]
+        public IActionResult Apagar()
+        {
+            try
+            {
+                // Ejecutar el comando de apagado en la máquina madre
+                using (var process = new Process())
+                {
+                    var startInfo = new ProcessStartInfo
+                    {
+                        FileName = "powershell.exe",
+                        Arguments = "Stop-Computer -Force",
+                        CreateNoWindow = true,
+                        WindowStyle = ProcessWindowStyle.Hidden
+                    };
+
+                    process.StartInfo = startInfo;
+                    process.Start();
+                    process.WaitForExit();
+                }
+
+                return Ok("Se ha enviado la solicitud de apagado.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error al enviar la solicitud de apagado: {ex.Message}");
+            }
+        }
+
+
+
+    }
+    public class DatoModelo
+    {
+        // Propiedades del modelo de datos
+        // ...
+    }
+}
